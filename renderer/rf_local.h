69e5263ba810a3ba5e83eb4ee1878a5582740365
Brad W. Williamson <bradtech519@outlook.com>
2026-01-06 21:31:46 -0600

    CI: add missing include guard to renderer/rf_local.h

--- renderer/rf_local.h
+++ renderer/rf_local.h
@@ -21,6 +21,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 // rf_local.h
 //
 
+#ifndef __RF_LOCAL_H__
+#define __RF_LOCAL_H__
+
 #include "r_local.h"
 
 #define ENTLIST_OFFSET	2
